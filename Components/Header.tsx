7482499a74bd5f4a4a717fcdaaf5e03d05c4a3f1
Harsh <57228178+harsh3294@users.noreply.github.com>
2022-05-28 12:19:30 +0530

    created header component

---
+++ Components/Header.tsx
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  MenuIcon,
+  SearchIcon,
+} from '@heroicons/react/solid'
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from '@heroicons/react/outline'
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+      <div className="relative h-12 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          src="/assets/images/reddit.png"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <div className=" mx-7 flex items-center xl:min-w-[130px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className=" ml-2 hidden flex-1 md:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search */}
+
+      <form className="roundend-sm flex flex-1 items-center space-x-2 border border-gray-200 bg-gray-100 px-3 py-1">
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button hidden type="submit" />
+      </form>
+      <div className="  mx-5 flex hidden items-center space-x-2 text-gray-500 md:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="border-gray=100 h-10 border" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center md:hidden">
+        <MenuIcon className="icon" />
+      </div>
+      <div className="hidden items-center space-x-2 border border-gray-100 p-2 md:flex">
+        <div className="relative h-8 w-6 flex-shrink-0">
+          <Image
+            src="/assets/images/signinlogo.png"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <p className="text-gray-500">Sign in</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
