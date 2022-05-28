b81b441e3b11339c612824e44285c68e7481f753
Harsh <57228178+harsh3294@users.noreply.github.com>
2022-05-28 12:54:35 +0530

    added reddit next-auth authentication

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -15,7 +15,9 @@ import {
   SpeakerphoneIcon,
   VideoCameraIcon,
 } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from 'next-auth/react'
 function Header() {
+  const { data: session } = useSession()
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-12 w-20 flex-shrink-0 cursor-pointer">
@@ -55,16 +57,39 @@ function Header() {
       <div className="ml-5 flex items-center md:hidden">
         <MenuIcon className="icon" />
       </div>
-      <div className="hidden items-center space-x-2 border border-gray-100 p-2 md:flex">
-        <div className="relative h-8 w-6 flex-shrink-0">
-          <Image
-            src="/assets/images/signinlogo.png"
-            layout="fill"
-            objectFit="contain"
-          />
+      {session ? (
+        <div
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 md:flex"
+          onClick={() => signOut()}
+        >
+          <div className="relative h-8 w-6 flex-shrink-0">
+            <Image
+              src="/assets/images/signinlogo.png"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-500">Sign Out</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        <p className="text-gray-500">Sign in</p>
-      </div>
+      ) : (
+        <div
+          className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 md:flex"
+          onClick={() => signIn()}
+        >
+          <div className="relative h-8 w-6 flex-shrink-0">
+            <Image
+              src="/assets/images/signinlogo.png"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <p className="text-gray-500">Sign in</p>
+        </div>
+      )}
     </div>
   )
 }
